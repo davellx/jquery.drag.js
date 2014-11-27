3a36eac07b8b6489482f29304e3f0d94897f8392
davel_x <davel_x@hotmail.com>
2014-11-27 17:53:19 +0100

    Create jquery.drag.js

---
+++ jquery.drag.js
@@ -0,0 +1,52 @@
+// Draggable plugin
+(function($) {
+    $.fn.drag = function(options) {
+        options = $.extend({
+            handle: null,
+            cursor: 'move',
+            onMoved:function(){}
+        }, options);
+
+        var $handle = this,
+            $drag = this;
+
+        if( options.handle ) {
+            $handle = $(options.handle);
+        }
+
+        $handle
+            .css('cursor', options.cursor)
+            .on("touchstart mousedown", function(e) {
+                var pos = e;
+                if(e.type === "touchstart"){
+                    pos = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+                }
+                var x = $drag.offset().left - pos.pageX,
+                    y = $drag.offset().top - pos.pageY,
+                    z = $drag.css('z-index');
+
+                $drag.css('z-index', 100000);
+
+                $(document.documentElement)
+                    .on('touchmove.drag mousemove.drag', function(e) {
+                        var pos = e;
+                        console.log('test');
+                        if(e.type === "touchmove"){
+                            pos = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+                        } 					
+                        $drag.offset({
+                            left: x + pos.pageX,
+                            top: y + pos.pageY
+                        });
+                        options.onMoved();
+                    })
+                    .on('touchend mouseup', function() {
+                        $(this).off('touchmove.drag mousemove.drag');
+                        $drag.css('z-index', z);
+                    });
+
+                // disable selection
+                e.preventDefault();
+            });
+    };
+})(jQuery);
